e1de0fae9bdd728af45bb05ee4d1468bb2cc8fa8
SebastianB <sebastianbjornerhag@gmail.com>
2020-08-04 10:38:02 +0200

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -10,8 +10,7 @@ def open_connection():
 	connection = getattr(g, "_connection", "None")
 
 	if connection == "None":
-		connection, g._connection = sqlite3.connect(PATH)
-		#connection = g._connection = sqlite3.connect(PATH)
+		connection = g._connection = sqlite3.connect(PATH)
 	connection.row_factory = sqlite3.Row
 
 	return connection
